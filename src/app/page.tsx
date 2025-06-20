594c27b28587a5cdca422cc5d50d5214798c1bc1
Tinkal300 <tinkal300@gmail.com>
2025-06-20 22:47:18 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="m-10">
+             <SignedOut>
+              <SignInButton />
+            </SignedOut>
+
+
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+  </div>
+  );
+}
